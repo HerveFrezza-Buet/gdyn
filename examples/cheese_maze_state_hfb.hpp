4b922904812e86f765f0aa3ab4576d3c018c23fd
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2023-07-31 17:48:41 +0200

    hfb

---
+++ examples/cheese_maze_state_hfb.hpp
@@ -0,0 +1,188 @@
+#pragma once // HFB: c'est pas standard stricto sensu... mais c'est mieux non ?
+
+
+#include <tuple>
+#include <random>
+
+#include <gdyn.hpp>
+
+// Simulator of the CheeseMaze, where the State can be observed.
+// observation = state + reward
+// command = Left/Right/Up/Down
+
+namespace cheese_maze { // C'est mieux, pour tes fonctions statiques à la to_string, la définition des types, etc. Utiliser une classe pour ses fonctions de namespace n'a d'intérêt, je trouve, que si ce qu'on définit dedans est private.
+
+  struct Parameters {
+    // amount of stochasticity in moving
+    double mishap_proba {0.1};
+  }; // struct Parameters
+
+  // HFB : Je ne m'occupe pas du générateur dans les params.
+
+
+  // J'aime bien les type Cell et Dir en dehors de la classe. En plus, comme ta classe est paramétrée a un paramètre de template, les nested types ::Dir diffèrent d'un paramétrage à l'autre, ce qui est mathématiquement faux.
+  
+  enum class Cell : int {C1 = 0, C2, C3, C4, C5, C6, C7, C8, C9, C10 , C11};
+  constexpr static int nbCell {static_cast<int>(Cell::C11)+1};
+  
+  static std::string to_string(const Cell& c ) {
+    std::stringstream sbuf;
+    sbuf << "C" << static_cast<int>(c)+1;
+    return sbuf.str();
+  }
+  
+  template<typename RANDOM_GENERATOR>
+  Cell random_cell(RANDOM_GENERATOR& gen) {
+    auto rnd_int = std::uniform_int_distribution<int>(0, nbCell-1)(gen);
+    return static_cast<Cell(rnd_int);
+  }
+  
+  enum class Dir : int {Left = 0, Right, Up, Down};
+  constexpr static int nbDir {static_cast<int>(Dir::Down)+1};
+  
+  static std::string to_string (const Dir& d ) {
+    switch (d) {
+    case Dir::Left:
+      return "L";
+    case Dir::Right:
+      return "R";
+    case Dir::Up:
+      return "U";
+    default:
+      return "D"; // HFB: d ne peut pas être autre chose... mais bon, en cas de bug, ton truc est peut-être mieux.
+    }
+  }
+
+  // Random state
+  template<typename RANDOM_GENERATOR>
+  Dir command_type random_command(RANDOM_GENERATOR& gen) {
+    switch(std::uniform_int_distribution<int>(0, nbDir-1)(gen)) {
+    case  0: return Dir::Left; break;
+    case  1: return Dir::Right; break;
+    case  2: return Dir::Up; break;
+    default: return Dir::Down; break;
+    }
+  }
+
+  template<typename RG>
+  class Environment { // HFB vu le namespace, on sait que c'est un cheese_maze, et j'aimais pas le nom state, confusant avec state_type.
+
+  public:
+
+    Parameters param; // HFB: Sans générateur, on peur copier les paramètres, et les garder comme attribut public.
+
+    mutable RG& gen; // Je le mets mutable pour qu'un const simulator puisse s'en servir.
+
+
+    // Transition as a vector of vector of neighbors
+    // BEWARE as Cell and Dir will be cast as int to get to results
+    std::vector<std::vector<Cell>> neighbors = {
+      {Cell::C1, Cell::C2, Cell::C1, Cell::C6},
+      {Cell::C1, Cell::C3, Cell::C2, Cell::C2},
+      {Cell::C2, Cell::C4, Cell::C3, Cell::C7},
+      {Cell::C3, Cell::C5, Cell::C4, Cell::C4},
+      {Cell::C4, Cell::C5, Cell::C5, Cell::C8},
+
+      {Cell::C6, Cell::C6, Cell::C1, Cell::C9},
+      {Cell::C7, Cell::C7, Cell::C3, Cell::C10},
+      {Cell::C8, Cell::C8, Cell::C5, Cell::C11},
+
+      {Cell::C9, Cell::C9, Cell::C6, Cell::C9},
+      {Cell::C10, Cell::C10, Cell::C7, Cell::C10},
+      {Cell::C11, Cell::C11, Cell::C8, Cell::C11},
+    };
+
+    // various spaces, as required by gdyn::specs::system.
+    using observation_type = std::tuple<Cell, double>;
+    using command_type     = Dir;
+    using state_type       = Cell;
+
+
+  private:
+
+    state_type state {Cell::C1};
+    double     reward {0};
+
+    // Does NOT take into account Bumping into walls
+    void compute_reward() {
+      if (state == Cell::C10) {
+	reward = 5;
+      }
+      else {
+	reward = 0;
+      }
+    }
+
+  public:
+
+    // HFB: Ta version était foireuse, tu n'avais pas besoin de templatifier le constructeur. Mais en plus, maintenant, on peut juste copier les paramètres.
+    Environment(const Parameters& param) : param(params) {}
+
+    // This is required by the gdyn::specs::system concept.
+    // This is for initializing the state of the system.
+    Environment& operator=(const state_type& init_state) {
+      state = init_state;
+      compute_reward();
+      return *this;
+    }
+
+    // This is required by the gdyn::specs::system concept.
+    // This returns the obsrvation corresponding to the system's state.
+    observation_type operator*() const {
+      return {state, reward};
+    }
+
+    // This is required by the gdyn::specs::system concept.
+    // This it true if the system is not in a terminal state.
+    operator bool() const {
+      return state != Cell::C10;
+    }
+
+    // This is required by the gdyn::specs::system concept.
+    // This performs a state transition.
+    // template<typename RANDOM_GENERATOR>
+    // void operator()(command_type command, RANDOM_GENERATOR& gen) {
+    void operator()(command_type command) {
+      if(*this) { // If we are not in a terminal state
+
+	auto prev_state = state;
+	// deterministic transition
+	state = neighbors.at(static_cast<int>(prev_state))
+	  .at(static_cast<int>(command));
+
+	// mishap ?
+	auto proba = std::uniform_real_distribution<double>(0,1)(gen);
+	if (proba < param.mishap_proba) {
+	  state = neighbors.at(static_cast<int>(prev_state))
+	    .at(std::uniform_int_distribution(0, nbDir-1)(gen));
+	}
+
+	// rewards for wall and cheese
+	if (prev_state == state) {
+	  reward = reward - 1;
+	}
+      }
+    }
+  }; // class Environment
+
+  // HFB: Ca devrait fixer tes issues je pense... N'oublie pas le inline !
+  // J'ai vire les const&... c'est des int ces trucs.
+  inline std::ostream& operator<<(std::ostream& os, Dir c)
+  {
+    os << "act=" << to_string(c);
+    return os;
+  }
+  
+  inline std::ostream& operator<<(std::ostream& os, Cell s)
+  {
+    os << "state=" << to_string(s);
+    return os;
+  }
+
+  template<typename RG>
+  void print_context(const std::string& msg,
+		     const typename Environement<RG>::state_type& state,
+		     double reward ) {
+    std::cout << msg << ": "<< to_string(state) << ", " << std::setw(3) << reward << std::endl;
+  }
+}
