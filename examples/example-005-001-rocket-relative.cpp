c4732a3a67b3eb96e72de0c7a978200ffd995aaf
Hervé Frezza-Buet <herve.frezza-buet@centralesupelec.fr>
2025-05-02 18:21:53 +0200

    updt

--- examples/example-005-001-rocket-relative.cpp
+++ examples/example-005-001-rocket-relative.cpp
@@ -1,71 +1,67 @@
 #include <iostream>
 #include <ranges>
-#include <iostream>
 #include <sstream>
-#include <gdyn.hpp>
 #include <fstream>
-#include <iterator>
-#include <array>
+#include <gdyn.hpp>
 
 
-#define END_OF_THRUST 9
+#define DT .1
 
 int main(int argc, char* argv[]) {
   std::random_device rd;
   std::mt19937 gen(rd());
   
   gdyn::problem::rocket::parameters params;
-  auto rocket = gdyn::problem::rocket::system(params);
-  gdyn::problem::rocket::thrust up   {.value = 12};
-  gdyn::problem::rocket::thrust none {};
-  gdyn::problem::rocket::phase init_state {.height = 30., .speed = -5.};
+  gdyn::problem::rocket::thrust up   {.value = 20, .duration = DT};
+  gdyn::problem::rocket::thrust none {.value =  0, .duration = DT};
+
+  auto rocket = gdyn::problem::rocket::system(params); // This knows its height and speed.
+  double target = 50; // This is the height we would like the rocket to stay at.
+  double t;
+
+  auto relative_rocket = gdyn::problem::rocket::relative::system(rocket, [&target](){return target;});
+
+  // Let us control the rocket roughly: if the error is negative, we apply up. We apply none otherwise.
+
+  std::cout << std::endl;
+  
+  {
+    std::string filename {"relative-rocket.dat"};
+    std::ofstream datafile {filename};
+    std::string targetname {"target.dat"};
+    std::ofstream datatarget {targetname};
+    t = 0;
+    relative_rocket = {.error = 10, .speed = 0};
+    for(auto [observation, action, report]
+	  : gdyn::views::controller(relative_rocket, [up, none](double error){if(error < 0) return up; return none;})
+	  | gdyn::views::orbit(rocket)
+	  | std::views::take(1000)) {
+      // We get orbits of rocket, while controlling relative_rocket. So
+      // we have access to height and speed, while relative rocket
+      // observations are only the current error.
+      datafile << t << ' ' << observation.height << ' ' << observation.speed << std::endl;
+      datatarget << t << ' ' << target << std::endl;
+      t += DT;
+      if(t > 35) target = 30; // We change the target.
+      if(t > 60) target = 50; // We change the target.
+    }
+    std::cout << "Generating " << filename << " and " << targetname << std::endl;
+  }
 
   
-  std::string mode = "nodrag";
-  for(int i = 0; i < 2; ++i) {
-    std::cout << std::endl;
+  std::string filename {"relative-rocket.plot"};
+  std::ofstream plotfile {filename};
 
-    // Using different dt shows that the dynamics is not implemented by
-    // an Euler integration when there is no drags. See the plottings.
-    for(auto dt : {.1, 1., 3.}) {
-      std::ostringstream filename;
-      filename << "rocket-" << mode << "-" << int(1000*dt) << "ms.dat";
-      std::ofstream datafile {filename.str()};
-      up.duration = dt;
-      none.duration = dt;
-      double t = 0;
-      rocket = init_state;
-      for(auto [observation, action, report]
-	    : gdyn::views::pulse([&up, &none, &t](){if(t < END_OF_THRUST - .01) return up; else return none;})
-	    | gdyn::views::orbit(rocket)) {
-	datafile << t << ' ' << observation.height << ' ' << observation.speed << std::endl;
-	t += dt;
-      }
-      std::cout << "Generating " << filename.str() << std::endl;
-    }
-    
-    std::ostringstream filename;
-    filename << "rocket-" << mode << ".plot";
-    std::ofstream datafile {filename.str()};
+  plotfile << "set yrange [0: 100]" << std::endl
+	   << "set trange [0:" << t << ']' << std::endl
+	   << "set parametric" << std::endl
+	   << "plot 'relative-rocket.dat' using 1:2 with lines lc rgb \"black\" title \"rocket height\", \\" << std::endl
+	   << "'target.dat' using 1:2 with lines lc rgb \"green\" title \"target\"" << std::endl;
+  std::cout << std::endl
+	    << "Run : gnuplot -p " << filename << std::endl
+	    << std::endl
+	    << std::endl;
 
-    datafile << "set yrange [-10: 100]" << std::endl
-	     << "set trange [-10: 100]" << std::endl
-	     << "set parametric" << std::endl
-	     << "plot " << END_OF_THRUST << ",t lc rgb \"green\" notitle, \\" << std::endl
-	     << "'rocket-" << mode << "-100ms.dat' using 1:2 with lines lc rgb \"black\" title \"0.1s\", \\" << std::endl
-	     << "'rocket-" << mode << "-1000ms.dat' using 1:2 with points pt 1 ps 2 lc rgb \"blue\" title \"1s\", \\" << std::endl
-	     << "'rocket-" << mode << "-3000ms.dat' using 1:2 with points pt 2 ps 2 lc rgb \"red\" title \"2.5s\"" << std::endl;
-
-    std::cout << std::endl
-	      << "Run : gnuplot -p " << filename.str() << std::endl
-	      << std::endl
-	      << std::endl;
-
-    // For the second run
-    mode = "drag";
-    params.drag_coef = .2;
-    rocket = params;
-  }
 
   return 0;
 }
